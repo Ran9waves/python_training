8bbf8b5528cc5f90865d1087effea6e87e4f2f99
ran9waves <ran9waves@protonmail.com>
2024-10-23 19:24:22 +0100

    improvements

--- web_server/webserver.py
+++ web_server/webserver.py
@@ -1,18 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app =  Flask(__name__) #equivalent of __main__
 
 @app.route('/') #gives extra tools to decoraate the server
 def hello_world():
+    print(url_for('static', filename='meh.ico'))
     return render_template('index.html')
 
 @app.route('/about.html') #gives extra tools to decoraate the server
 def about():
     return render_template('about.html')
 
-@app.route('/blog') #gives extra tools to decoraate the server
-def blog():
-    return 'Welcome to my cult'
+#@app.route('/favicon.ico') 
+#def favicon():
+#    return 'Welcome to my cult'
 
 @app.route('/blog/2020/mantis_shrimp') #gives extra tools to decoraate the server
 def blog2():
