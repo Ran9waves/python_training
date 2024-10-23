512a973acc1fed094b7bb4c8728ef242d629fc7e
ran9waves <ran9waves@protonmail.com>
2024-10-23 21:40:58 +0100

    website project final

--- web_server/webserver.py
+++ web_server/webserver.py
@@ -2,18 +2,14 @@ from flask import Flask, render_template, url_for
 
 app =  Flask(__name__) #equivalent of __main__
 
-@app.route('/') #gives extra tools to decoraate the server
-def hello_world():
-    print(url_for('static', filename='meh.ico'))
+@app.route('/') #gives extra tools to decorate the server like user name and the post id in int format
+def my_home():
     return render_template('index.html')
 
 @app.route('/about.html') #gives extra tools to decoraate the server
 def about():
     return render_template('about.html')
 
-#@app.route('/favicon.ico') 
-#def favicon():
-#    return 'Welcome to my cult'
 
 @app.route('/blog/2020/mantis_shrimp') #gives extra tools to decoraate the server
 def blog2():
